736cad575632a3685f3eed6e2e99d8c20134729b
Harlon Garcia <harloongarcia@gmail.com>
2024-01-14 13:33:53 -0300

    project initialized

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Chronos is running!")
+  .listen(process.env.PORT || 8000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+console.warn(
+  `🦊 Chronos is running at ${app.server?.hostname}:${app.server?.port}`
 );
